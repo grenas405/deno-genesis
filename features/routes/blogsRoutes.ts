da44826a411163406ba2a26d3a1fc040be468f88
Pedro M. Dominguez <domingueztechsolutions@gmail.com>
2025-09-16 10:50:50 -0500

    Create blogsRoutes.ts

---
+++ features/routes/blogsRoutes.ts
@@ -0,0 +1,24 @@
+import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import {
+  getAllBlogs,
+  createBlogPost,
+  updateBlogPost,
+  deleteBlogPost,
+} from "../controllers/blogsController.ts";
+import { verifyAdminToken } from "../middleware/authMiddleware.ts";
+
+const router = new Router();
+
+// GET /api/blogs (public)
+router.get("/", getAllBlogs);
+
+// POST /api/blogs (admin protected)
+router.post("/", verifyAdminToken, createBlogPost);
+
+// PUT /api/blogs/:id (admin protected)
+router.put("/:id", verifyAdminToken, updateBlogPost);
+
+// DELETE /api/blogs/:id (admin protected)
+router.delete("/:id", verifyAdminToken, deleteBlogPost);
+
+export default router;
