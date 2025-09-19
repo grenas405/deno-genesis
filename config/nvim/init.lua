2e51753c0c0b0d775ade5292e818222d4660592b
Pedro M. Dominguez <domingueztechsolutions@gmail.com>
2025-09-19 16:08:02 -0500

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,50 +9,10 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
 
 -- Load core configuration
+require("config.lazy")
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 
--- Setup plugins with performance optimizations
-require("lazy").setup("plugins", {
-  defaults = {
-    lazy = false,
-    version = false,
-  },
-  performance = {
-    cache = {
-      enabled = true,
-    },
-    reset_packpath = true,
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
-  install = { colorscheme = { "tokyonight" } },
-  checker = { enabled = true, notify = false },
-  change_detection = { notify = false },
-})
